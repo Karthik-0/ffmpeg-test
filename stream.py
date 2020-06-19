4107d77a81679bc61b933d98a98834a49506c379
Karthik <karthik@testpress.in>
2020-06-19 19:32:05 +0530

    Change dir

--- stream.py
+++ stream.py
@@ -28,8 +28,8 @@ s3_file = S3File(s3_object)
 
 command = "ffmpeg -i - -preset ultrafast -b:a 128k  -map 0:0 -map 0:1 -map 0:0 -map 0:1 -map 0:0 -map 0:1 -s:v:0 " \
           "640x360 -b:v:0 400k  -s:v:1 960x540 -b:v:1 600k -s:v:2 1280x720 -c:v:2 " \
-          "libx265 -b:v:1 1500k -var_stream_map 'v:0,a:0 v:1,a:1 v:2,a:2'  -master_pl_name small_video2/master.m3u8  -f hls " \
-          "-hls_time 6 -hls_list_size 0 -hls_flags temp_file  small_video2/segement%v/video.m3u8"
+          "libx265 -b:v:1 1500k -var_stream_map 'v:0,a:0 v:1,a:1 v:2,a:2'  -master_pl_name small_video3/master.m3u8  -f hls " \
+          "-hls_time 6 -hls_list_size 0 -hls_flags temp_file  small_video3/segement%v/video.m3u8"
 
 
 
@@ -52,7 +52,7 @@ def upload_videos(line, exclude_m3u8=False):
 
     regex_pattern = re.compile("(Opening .* for writing)")
     if regex_pattern.search(line):
-        upload_dir("video", exclude_files=exclude_files)
+        upload_dir("small_video3", exclude_files=exclude_files)
 
 
 def monitor(ffmpeg, duration, time_, process):
@@ -98,6 +98,6 @@ if __name__ == "__main__":
     print("Start Transcoding : ", AWS_SECRET_ACCESS_KEY)
     # input_stream()
     process_poc()
-    upload_dir("video")
+    upload_dir("small_video3")
 
 #  https://s3-ap-southeast-1.amazonaws.com/media.testpress.in/institute/sandbox/videos/232ae54d31614f3f95c46b2dce2c2975.mp4
