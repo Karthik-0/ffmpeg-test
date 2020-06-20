b32da03f4c873f1b4682d93843cb58073110ee36
Karthik <karthik@testpress.in>
2020-06-20 13:45:46 +0530

    Big vide

--- stream.py
+++ stream.py
@@ -19,17 +19,17 @@ s3 = boto3.resource("s3", aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
                     aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
                     region_name="ap-southeast-1")
 
-s3_object = s3.Object(bucket_name="media.testpress.in",
-                      key="institute/sandbox/videos/232ae54d31614f3f95c46b2dce2c2975.mp4")
 # s3_object = s3.Object(bucket_name="media.testpress.in",
-#                       key="institute/institute/demo/1c61861f5875407fa2e3c1531ef6a602.mp4")
+#                       key="institute/sandbox/videos/232ae54d31614f3f95c46b2dce2c2975.mp4")
+s3_object = s3.Object(bucket_name="media.testpress.in",
+                      key="institute/institute/demo/1c61861f5875407fa2e3c1531ef6a602.mp4")
 
 s3_file = S3File(s3_object)
 
-command = "ffmpeg -i - -preset fast -b:a 128k -map 0:0 -map 0:1 -map 0:0 -map 0:1 -map 0:0 -map 0:1 -s:v:0 " \
+command = "ffmpeg -i - -preset faster -b:a 128k -map 0:0 -map 0:1 -map 0:0 -map 0:1 -map 0:0 -map 0:1 -s:v:0 " \
           "640x360 -c:v:0 libx264 -b:v:0 400k -s:v:1 960x540 -c:v:1 libx264 -b:v:1 600k -s:v:2 1280x720 " \
-          "-b:v:2 1500k -c:v:2 libx264 -var_stream_map 'v:0,a:0 v:1,a:1 v:2,a:2'  -master_pl_name small_video9/master.m3u8  -f hls " \
-          "-hls_time 6 -hls_list_size 0 -hls_flags temp_file  small_video9/segement%v/video.m3u8"
+          "-b:v:2 1500k -c:v:2 libx264 -var_stream_map 'v:0,a:0 v:1,a:1 v:2,a:2'  -master_pl_name big_video_faster/master.m3u8  -f hls " \
+          "-hls_time 6 -hls_list_size 0 -hls_flags temp_file  big_video_faster/segement%v/video.m3u8"
 
 
 
@@ -52,7 +52,7 @@ def upload_videos(line, exclude_m3u8=False):
 
     regex_pattern = re.compile("(Opening .* for writing)")
     if regex_pattern.search(line):
-        upload_dir("small_video9", exclude_files=exclude_files)
+        upload_dir("big_video_faster", exclude_files=exclude_files)
 
 
 def monitor(ffmpeg, duration, time_, process):
@@ -98,6 +98,6 @@ if __name__ == "__main__":
     print("Start Transcoding : ", AWS_SECRET_ACCESS_KEY)
     # input_stream()
     process_poc()
-    upload_dir("small_video9")
+    upload_dir("big_video_faster")
 
 #  https://s3-ap-southeast-1.amazonaws.com/media.testpress.in/institute/sandbox/videos/232ae54d31614f3f95c46b2dce2c2975.mp4
